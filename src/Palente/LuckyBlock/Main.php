8b98e046dbf8d71d3bc93c0ee02cdf561ae63f68
Bluzzi <bluzzigame@gmail.com>
2019-04-28 21:06:26 +0200

    Beautify :heart:

--- src/Palente/LuckyBlock/Main.php
+++ src/Palente/LuckyBlock/Main.php
@@ -1,59 +1,79 @@
 <?php
-#c'est un bon début
-namespace Palente\LuckyBlock; 
-use pocketmine\Player;
+
+namespace Palente\LuckyBlock;
+
 use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+
 use pocketmine\Server;
 use pocketmine\utils\TextFormat as TX;
+
 use pocketmine\utils\Config;
+
 use Palente\LuckyBlock\utils\Events;
-class Main extends PluginBase
-{
+
+class Main extends PluginBase {
+
 	public static $instance;
 	public static $logger = null;
+
 	public $mode_eco = false;
 	public $mode_enc = false;
+
 	public $prefix = TX::BLUE."[".TX::AQUA."LuckyBlock".TX::BLUE."] ".TX::RESET;
+
 	public function onLoad(){
 		self::$logger = $this->getLogger();
 		self::$instance =$this;
 	}
+
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+
 		@mkdir($this->getDataFolder());
+
 		if(!file_exists($this->getDataFolder()."config.yml")){
 			$this->saveResource('config.yml');
 		}
+
 		$this->config = new Config($this->getDataFolder().'config.yml', Config::YAML);
+
 		if($this->config->get("usage_of_EconomyAPI") == "true"){
 			#Maybe one day add other API
 			if(!$this->getServer()->getPluginManager()->getPlugin('EconomyAPI')){
 				self::$logger->error('You have enabled the usage of the plugin EconomyAPI but the plugin is not found');
 				$this->isEnabled = false;
 				$this->getServer()->getPluginManager()->disablePlugin($this);
-				#
-			}else {$this->EconomyAPI= $this->getServer()->getPluginManager()->getPlugin('EconomyAPI');
+			}else {
+				$this->EconomyAPI= $this->getServer()->getPluginManager()->getPlugin('EconomyAPI');
 				$this->mode_eco = true;
 			}
-		}else $this->mode_eco = false;
+		} else {
+			$this->mode_eco = false;
+		}
+
 		if($this->config->get("usage_of_PiggyCustomEnchants") == "true"){
 			#Maybe one day add other API
 			if(!$this->getServer()->getPluginManager()->getPlugin('PiggyCustomEnchants')){
 				self::$logger->error('You have enabled the usage of the plugin PiggyCustomEnchants but the plugin is not found');
 				$this->isEnabled = false;
 				$this->getServer()->getPluginManager()->disablePlugin($this);
-				#
 			}else {
 				$this->piggy= $this->getServer()->getPluginManager()->getPlugin('PiggyCustomEnchants');
 				$this->mode_enc = true;
 			}
-		}else $this->mode_enc = false;
+		} else {
+			$this->mode_enc = false;
+		}
+
 		/*
 		#For later if i will do a critical change in config
 		if($this->config->get("version") !== $this->getDescription()->version){
 			self::$logger->warning("Critical changes have been made in the new version of the plugin and it seem that your config.yml is a older config. Please delete your config.yml and restart your server");
 		}*/
 	}
+
 	public static function getInstance(){
 		return self::$instance;
 	}
--- src/Palente/LuckyBlock/utils/Events.php
+++ src/Palente/LuckyBlock/utils/Events.php
@@ -1,23 +1,31 @@
 <?php
+
 namespace Palente\LuckyBlock\utils;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\utils\TextFormat as TX;
-use Palente\LuckyBlock\Main as MN;
-#Events
+
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
-#Other
-use pocketmine\math\Vector3;
+
+use pocketmine\Player;
+
+use pocketmine\Server;
+
 use pocketmine\item\Item;
+
 use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
+
 use pocketmine\command\ConsoleCommandSender;
-class Events implements Listener
-{
-    public $eco = null;
-    public $caller = null;
-    public $cnf = null;
+
+use pocketmine\math\Vector3;
+
+use Palente\LuckyBlock\Main as MN;
+
+class Events implements Listener {
+
+    public $eco;
+    public $caller;
+	public $cnf;
+	
     public function __construct(MN $caller){
     	$this->caller = $caller;
     }
@@ -25,26 +33,29 @@ class Events implements Listener
     public function onBreak(BlockBreakEvent $event){
     	$block = $event->getBlock();
     	$player = $event->getPlayer();
-    	$cnf = $this->caller->config;
-	#..... yeah i forget it, How idk
-	if($event->isCancelled()) return;
+		$cnf = $this->caller->config;
+		
+		if($event->isCancelled()) return;
+
     	if($block->getId() == $this->caller->config->get('LuckyBlockId')){
     		#21 possibility
     		$nbchance = mt_rand(0, 20);
-    		$obj = $cnf->get("Chance-".$nbchance);
+			$obj = $cnf->get("Chance-".$nbchance);
+			
     		if(!isset($obj['Type'])){
     			$player->sendPopup($this->caller->prefix."Anything winned.");
     			$event->setDrops([Item::get(0,0,0)]);		
-    		}
+			}
+			
 			if($obj['Type'] == "items"){
     			$item = $obj['idItems'];
     			$amount = $obj['amountItems'];
     			$event->setDrops([Item::get($item,0,$amount)]);
     			$player->sendPopup("You winned Item!!");
-    			return;
-    		}elseif(strtolower($obj['Type']) == "blocks"){
+				return;
+    		} elseif(strtolower($obj['Type']) == "blocks"){
     			$theblock = $obj['idBlocks'];
-    			$block->getLevel()->setBlock($block/*->asPosition()->asVector3()*/,Block::get($theblock), true);
+    			$block->getLevel()->setBlock($block->asPosition()->asVector3(), Block::get($theblock), true);
     			$event->setCancelled();
     			$event->setDrops([Item::get(0,0,0)]);
     			return;
@@ -55,28 +66,31 @@ class Events implements Listener
     				$player->sendMessage($this->caller->prefix."You winned ".$money." money! §aCongratulation!§a");
     				$event->setDrops([Item::get(0,0,0)]);
     				return;
-    			}else{
-    				MN::$logger->warning('Usage of The type money in the case '.$nbchance.' but economy is disabled..');
-    				$player->sendMessage($this->caller->prefix."Oups.. Error has occured.. No gain found");
-    				$event->setDrops([Item::get(0,0,0)]);
-    				return;
-    				}
-    		}elseif(strtolower($obj['Type']) ==  "commands"){
-    			$event->setDrops([Item::get(0,0,0)]);
+    			} else {
+					MN::$logger->warning('Usage of The type money in the case '.$nbchance.' but economy is disabled..');
+					$player->sendMessage($this->caller->prefix."Oups.. Error has occured.. No gain found");
+					$event->setDrops([Item::get(0,0,0)]);
+					return;
+    			}
+    		} elseif(strtolower($obj['Type']) ==  "commands"){
+				$event->setDrops([Item::get(0,0,0)]);
+				
     			$cmd = $obj['command'];
-    			$cmd = str_replace(":nameofplayer:", $player->getName(), $cmd);
-    			if ($obj['executor'] == "player") {
+				$cmd = str_replace(":nameofplayer:", $player->getName(), $cmd);
+				
+    			if($obj['executor'] == "player"){
     				$this->caller->getServer()->dispatchCommand($player, $cmd);
     				$player->sendPopup($this->caller->prefix."executing command..");
-    			}elseif ($obj['executor'] ==  "console") {
+    			} elseif ($obj['executor'] ==  "console"){
     				$this->caller->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
     				$player->sendPopup($this->caller->prefix."executing command..");
-    			}else{
+    			} else {
     				MN::$logger->warning('Usage of The type command in the case '.$nbchance.' but the executor is not player or command it\'s '.$obj['executor']);
     				$player->sendMessage($this->caller->prefix."Oups.. error has occured.. No gain found for Commands");
-    			}
+				}
+				
     			return;
-    		}elseif(strtolower($obj['Type']) =="enchant"){
+    		} elseif(strtolower($obj['Type']) =="enchant"){
                 if($this->caller->mode_enc && isset($obj['idItems'],$obj['amountItems'], $obj['enchantName'], $obj['enchantLevel'])){
                     $item = $obj['idItems'];
                     $amount = $obj['amountItems'];
@@ -86,7 +100,7 @@ class Events implements Listener
                     $this->caller->piggy->addEnchantment($item, $enc, $encl);
                     $event->setDrops([$item]);
                     $player->sendPopup($this->caller->prefix."You get an enchanted item");
-                }else{
+                } else {
                     MN::$logger->warning('Usage of The type enchant in the case '.$nbchance.' but one of them is empty OR Piggy is not available');
                     $player->sendMessage($this->caller->prefix."Oups.. error has occured.. No gain found for Enchant");
                 }
@@ -94,6 +108,6 @@ class Events implements Listener
     			$player->sendPopup($this->caller->prefix."Anything winned.");
     			$event->setDrops([Item::get(0,0,0)]);
     		}
-    	}else return;
+    	} else return;
     }
 }
