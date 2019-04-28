84bddfe30952a2c847592782ed5bb54dac8dcf65
Bluzzi <bluzzigame@gmail.com>
2019-04-28 22:36:18 +0200

    Using switch for cleaner code

--- src/palente/luckyblock/Events.php
+++ src/palente/luckyblock/Events.php
@@ -33,81 +33,87 @@ class Events implements Listener {
     public function onBreak(BlockBreakEvent $event){
     	$block = $event->getBlock();
     	$player = $event->getPlayer();
-		$cnf = $this->caller->config;
+		$config = $this->caller->config;
 		
 		if($event->isCancelled()) return;
 
-    	if($block->getId() == $this->caller->config->get('LuckyBlockId')){
-    		#21 possibility
-    		$nbchance = mt_rand(0, 20);
-			$obj = $cnf->get("Chance-".$nbchance);
-			
-    		if(!isset($obj['Type'])){
-    			$player->sendPopup($this->caller->prefix."Anything winned.");
-    			$event->setDrops([Item::get(0,0,0)]);		
-			}
-			
-			if($obj['Type'] == "items"){
-    			$item = $obj['idItems'];
-    			$amount = $obj['amountItems'];
-    			$event->setDrops([Item::get($item,0,$amount)]);
-    			$player->sendPopup("You winned Item!!");
+		if($block->getId() == $this->caller->config->get("LuckyBlockId")){
+			$nbchance = mt_rand(0, 20);
+			$loot = $config->get("Chance-" . $nbchance);
+
+			if(empty($loot["Type"])){
+				$player->sendPopup($this->caller->prefix . "Anything winned.");
+    			$event->setDrops(array(Item::get(0, 0, 0));
+
 				return;
-    		} elseif(strtolower($obj['Type']) == "blocks"){
-    			$theblock = $obj['idBlocks'];
-    			$block->getLevel()->setBlock($block->asPosition()->asVector3(), Block::get($theblock), true);
-    			$event->setCancelled();
-    			$event->setDrops([Item::get(0,0,0)]);
-    			return;
-    		}elseif(strtolower($obj['Type']) == "money"){
-    			if($this->caller->mode_eco){
-    				$money = $obj["moneyToAdd"];
-    				$this->caller->EconomyAPI->addMoney($player,$money);
-    				$player->sendMessage($this->caller->prefix."You winned ".$money." money! §aCongratulation!§a");
-    				$event->setDrops([Item::get(0,0,0)]);
-    				return;
-    			} else {
-					MN::$logger->warning('Usage of The type money in the case '.$nbchance.' but economy is disabled..');
-					$player->sendMessage($this->caller->prefix."Oups.. Error has occured.. No gain found");
-					$event->setDrops([Item::get(0,0,0)]);
-					return;
-    			}
-    		} elseif(strtolower($obj['Type']) ==  "commands"){
-				$event->setDrops([Item::get(0,0,0)]);
-				
-    			$cmd = $obj['command'];
-				$cmd = str_replace(":nameofplayer:", $player->getName(), $cmd);
-				
-    			if($obj['executor'] == "player"){
-    				$this->caller->getServer()->dispatchCommand($player, $cmd);
-    				$player->sendPopup($this->caller->prefix."executing command..");
-    			} elseif ($obj['executor'] ==  "console"){
-    				$this->caller->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
-    				$player->sendPopup($this->caller->prefix."executing command..");
-    			} else {
-    				MN::$logger->warning('Usage of The type command in the case '.$nbchance.' but the executor is not player or command it\'s '.$obj['executor']);
-    				$player->sendMessage($this->caller->prefix."Oups.. error has occured.. No gain found for Commands");
-				}
-				
-    			return;
-    		} elseif(strtolower($obj['Type']) =="enchant"){
-                if($this->caller->mode_enc && isset($obj['idItems'],$obj['amountItems'], $obj['enchantName'], $obj['enchantLevel'])){
-                    $item = $obj['idItems'];
-                    $amount = $obj['amountItems'];
-                    $item = Item::get($item,0,$amount);
-                    $enc = $obj['enchantName'];
-                    $encl = $obj['enchantLevel'];
-                    $this->caller->piggy->addEnchantment($item, $enc, $encl);
-                    $event->setDrops([$item]);
-                    $player->sendPopup($this->caller->prefix."You get an enchanted item");
-                } else {
-                    MN::$logger->warning('Usage of The type enchant in the case '.$nbchance.' but one of them is empty OR Piggy is not available');
-                    $player->sendMessage($this->caller->prefix."Oups.. error has occured.. No gain found for Enchant");
-                }
-            }else{
-    			$player->sendPopup($this->caller->prefix."Anything winned.");
-    			$event->setDrops([Item::get(0,0,0)]);
-    		}
-    	} else return;
+			}
+
+			switch($loot["Type"]){
+				case "items":
+					$item = $loot["idItems"];
+					$amount = $loot["amountItems"];
+
+					$event->setDrops(array(Item::get($item, 0, $amount));
+					$player->sendPopup("You winned Item !!");
+				break;
+
+				case "blocks":
+					$theblock = $loot["idBlocks"];
+
+					$block->getLevel()->setBlock($block->asPosition()->asVector3(), Block::get($theblock), true);
+					$event->setDrops(array(Item::get(0, 0, 0));
+					$event->setCancelled();
+				break;
+
+				case "money":
+					if($this->caller->mode_eco){
+						$money = $loot["moneyToAdd"];
+
+						$this->caller->EconomyAPI->addMoney($player, $money);
+						$player->sendMessage($this->caller->prefix."You winned ".$money." money! §aCongratulation!§a");
+						$event->setDrops(array(Item::get(0, 0, 0));
+					} else {
+						MN::$logger->warning("Usage of The type money in the case ".$nbchance." but economy is disabled..");
+						$player->sendMessage($this->caller->prefix."Oups.. Error has occured.. No gain found");
+						$event->setDrops(array(Item::get(0, 0, 0));
+					}
+				break;
+
+				case "commands":
+					$cmd = $loot["command"];
+					$cmd = str_replace(":nameofplayer:", $player->getName(), $cmd);
+					
+					if($loot["executor"] == "player"){
+						$this->caller->getServer()->dispatchCommand($player, $cmd);
+						$player->sendPopup($this->caller->prefix."executing command..");
+					} elseif ($loot["executor"] ==  "console"){
+						$this->caller->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
+						$player->sendPopup($this->caller->prefix."executing command..");
+					} else {
+						MN::$logger->warning("Usage of The type command in the case ".$nbchance." but the executor is not player or command it\"s ".$loot["executor"]);
+						$player->sendMessage($this->caller->prefix."Oups.. error has occured.. No gain found for Commands");
+					}
+
+					$event->setDrops(array(Item::get(0, 0, 0));
+				break;
+
+				case "enchant":
+					if($this->caller->mode_enc && isset($loot["idItems"], $loot["amountItems"], $loot["enchantName"], $loot["enchantLevel"])){
+						$item = $loot["idItems"];
+						$amount = $loot["amountItems"];
+						$item = Item::get($item, 0, $amount);
+						$enc = $loot["enchantName"];
+						$encl = $loot["enchantLevel"];
+						$this->caller->piggy->addEnchantment($item, $enc, $encl);
+						$event->setDrops([$item]);
+						$player->sendPopup($this->caller->prefix."You get an enchanted item");
+					} else {
+						MN::$logger->warning("Usage of The type enchant in the case ".$nbchance." but one of them is empty OR Piggy is not available");
+						$player->sendMessage($this->caller->prefix."Oups.. error has occured.. No gain found for Enchant");
+					}
+				break;
+			}
+
+		}
     }
 }
