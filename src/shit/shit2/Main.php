9e3f561365858a23dc81fdcc568d41a4a6470494
Bluzzi <bluzzigame@gmail.com>
2019-04-29 00:11:49 +0200

    Folder bug (1/2)

---
+++ src/shit/shit2/Main.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Palente\LuckyBlock;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+
+use pocketmine\Server;
+
+use pocketmine\utils\Config;
+
+use Palente\LuckyBlock\utils\Events;
+
+class Main extends PluginBase {
+
+	/** @var $main and $config instances */
+    public static $main, $config;
+
+	/** @var $economyPlugin and $mode_eco economyAPI plugin variables */
+	public $economyPlugin;
+	public $mode_eco = false;
+
+	/** @var $piggyPlugin and $mode_enc PiggyCustomEnchant plugin variables */
+	public $piggyPlugin;
+	public $mode_enc = false;
+
+	/** @var $prefix the prefix */
+	public $prefix = "§e[§bLuckyBlock§e]§r" . " ";
+
+	/**
+	 * When the plugin is started.
+	 */
+	public function onEnable(){
+		# Register events class:
+		$this->getServer()->getPluginManager()->registerEvents(new Events(), $this);
+
+		# Creating the configuration if it is not done and updating it:
+		if(file_exists($this->getDataFolder() . "config.yml")){
+            if(self::getDefaultConfig()->get("version") !== $this->getVersion()){
+				$this->getLogger()->warning("Critical changes have been made in the new version of the plugin and it seem that your config.yml is a older config. Please delete your config.yml and restart your server.");
+				//TODO: ideas for the method used to update the configuration ?
+			}
+		} else {
+			$this->saveResource("config.yml");
+		}
+		
+		# Register statics:
+		self::$main = $this;
+		self::$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+
+		# Enabling the use of the EconomyAPI plugin:
+		if(self::getDefaultConfig()->get("usage_of_EconomyAPI") == "true"){
+			if($this->getServer()->getPluginManager()->getPlugin("EconomyAPI")){
+				$this->economyPlugin = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+				$this->mode_eco = true;
+			} else {
+				$this->getLogger()->error("You have enabled the usage of the plugin EconomyAPI but the plugin is not found.");
+			}
+		}
+
+		# Enabling the use of the PiggyCustomEnchant plugin:
+		if(self::getDefaultConfig()->get("usage_of_PiggyCustomEnchants") == "true"){
+			if($this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants")){
+				$this->piggyPlugin = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
+				$this->mode_enc = true;
+			} else {
+				$this->getLogger()->error("You have enabled the usage of the plugin PiggyCustomEnchants but the plugin is not found.");
+			}
+		}
+	}
+
+	/**
+	 * Return instance of Main class.
+	 * @return Main
+	 */
+	public static function getInstance() : Main {
+		return self::$main;
+	}
+
+	/**
+     * Return instance of plugin config.
+     * @return Config
+     */
+    public static function getDefaultConfig() : Config {
+        return self::$config;
+    }
+}
